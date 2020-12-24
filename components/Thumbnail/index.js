ee264f3296fef5f967210bb48b033db3a1c458de
junaidahmedvd <junaid.ahmed@venturedive.com>
2020-12-24 17:42:27 +0500

    next test app

---
+++ components/Thumbnail/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Thumbnail = ({
+  imageUrl = 'https://via.placeholder.com/210x295?text=?',
+  caption,
+  href = '',
+  as = '',
+  small = false
+}) => {
+  return (
+    <div className='thumbnail'>
+      <Link href={href} as={as}>
+        <a>
+          <img src={imageUrl} className='thumbnail__image' />
+          <div className='thumbnail__caption'>{caption}</div>
+        </a>
+      </Link>
+
+      <style jsx>{`
+        .thumbnail__image {
+          width: ${small ? '100px' : '100%'};
+        }
+        .thumbnail__caption {
+          text-align: center;
+          padding: 10px;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default Thumbnail;
---
+++ pages/[country]/index.js
@@ -0,0 +1,65 @@
+import axios from 'axios';
+import Error from 'next/error';
+import nookies from 'nookies';
+import Thumbnail from '../../components/Thumbnail';
+
+const Home = ({ shows, country, statusCode }) => {
+  if (statusCode) {
+    return <Error statusCode={statusCode} />;
+  }
+
+  const renderShows = () => {
+    return shows.map((showItem, index) => {
+      const { show } = showItem;
+
+      return (
+        <li key={index}>
+          <Thumbnail
+            imageUrl={(show.image && show.image.medium) || undefined}
+            caption={show.name}
+            href='/[country]/[showId]'
+            as={`/${country}/${show.id}`}
+          />
+        </li>
+      );
+    });
+  };
+
+  return (
+    <div className='home'>
+      <ul className='tvshows-grid'>
+        {renderShows()}
+
+        <style jsx>{`
+          .tvshows-grid {
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            gap: 10px;
+          }
+        `}</style>
+      </ul>
+    </div>
+  );
+};
+
+Home.getInitialProps = async context => {
+  try {
+    const { defaultCountry } = nookies.get(context);
+    const country = context.query.country || defaultCountry || 'us';
+
+    const response = await axios.get(
+      `https://api.tvmaze.com/schedule?country=${country}&date=2014-12-01`
+    );
+
+    return {
+      shows: response.data,
+      country
+    };
+  } catch (error) {
+    return {
+      statusCode: error.response ? error.response.status : 500
+    };
+  }
+};
+
+export default Home;
